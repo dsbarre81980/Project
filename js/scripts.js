3fa9eb10dc47a7d6bd239a99eee98c4bd31b1fc0
David Sebastian Barrera Gaona <102325692+dsbarre81980@users.noreply.github.com>
2025-02-28 21:31:31 -0500

    Cambio Descripcion con js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 const card = document.querySelector('.container');
+const description = document.getElementById('description');
+const changeTextButton = document.getElementById('changeTextButton');
+
+const text1 = "Ingeniero de sistemas, apasionado por los datos, AWS, desarrollo web, nuevas tecnologías y el mundo de la computación.";
+const text2 = "Seminarista anglicano, tengo novia y planeo casarme en 3 años.";
+
+let isText1 = true;
 
 window.addEventListener('load', () => {
     card.classList.add('highlight');
@@ -6,4 +13,13 @@ window.addEventListener('load', () => {
     card.addEventListener('animationend', () => {
         card.classList.remove('highlight');
     });
+});
+
+changeTextButton.addEventListener('click', () => {
+    if (isText1) {
+        description.textContent = text2;
+    } else {
+        description.textContent = text1;
+    }
+    isText1 = !isText1; 
 });
